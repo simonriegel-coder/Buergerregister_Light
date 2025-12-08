9affdcbe22384e259f6c4879b4ba447b926c6205
Hilmanhaq <hilmanhaq2001@gmail.com>
2025-12-08 21:29:29 +0100

    Coder Verfeinung

--- src/buergerregister/models.py
+++ src/buergerregister/models.py
@@ -29,6 +29,14 @@ class Person:
     def get_full_name(self) -> str:
         """Gibt den vollständigen Namen zurück."""
         return f"{self.vorname} {self.nachname}"
+    
+    def get_wohnort(self) -> str:
+         """Gibt den Wohnort Zurück."""
+         return self.wohnort
+    
+    def get_geburtsjahr(self) -> int:
+            """Gibt das Geburtsjahr Zurück."""
+            return self.geburtsjahr
         
 
   
